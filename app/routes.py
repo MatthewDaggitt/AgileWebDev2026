37c0bebbbc268b2d8402e811697756f52ff5333b
MatthewDaggitt <matthewdaggitt@gmail.com>
2026-04-24 09:45:52 +0800

    Added very basic flask setup

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template
+
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/page')
+def bootstrap():
+    return render_template('bootstrap.html')
